ed4e1895034a4d67d970736e060001b28f824627
Kookiat <kookiatsuetrong@gmail.com>
2020-01-17 20:58:42 +0700

    Add log in mechanism

--- main.js
+++ main.js
@@ -5,15 +5,29 @@ var source  = { user: 'james',     password: 'bond',
                 host: '127.0.0.1', database: 'magic', port: 3306}
 // var source  = 'mysql://james:bond@127.0.0.1:3306/magic'
 var pool    = mysql.createPool(source)
-
 var server  = express() // create server
 server.engine('html', ejs.renderFile)
+var readBody = express.urlencoded({extended:false})
 server.listen(2000)
 server.get('/api/list-product', listProduct)
 server.get('/', showHome)
 server.get('/login', showLogInPage)
+server.post('/login', readBody, checkPassword)
 server.get('/detail', showDetail)
 server.get('/search', showSearchResult)
+
+function checkPassword(req, res) {
+    var sql  = 'select * from magic.member where ' +
+                ' email=? and password=sha2(?,512)'
+    var data = [ req.body.email, req.body.password ]
+    pool.query(sql, data, function(error, data) {
+        if (data.length == 1) {
+            res.send('Pass')
+        } else {
+            res.send('Fail')
+        }
+    })
+}
 
 function listProduct(req, res) {
     pool.query('select * from product', function(error, data) {
